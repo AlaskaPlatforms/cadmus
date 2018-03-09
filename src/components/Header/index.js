7caa74a922006363d0006ebe08359d5b68210d32
Pedro Henrique <pedro.dorini@hotmail.com>
2018-03-09 20:51:13 -0300

    adicionar icone na logo da navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,13 +5,13 @@ export default class Header extends Component {
     return (
       <nav>
         <div class="nav-wrapper blue darken-4">
-          <a href="#" class="brand-logo">Cadmus</a>
+          <a href="#" class="brand-logo"><i class="material-icons">book</i>Cadmus</a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a>Sign in</a></li>
             <li><a>Sign up</a></li>
           </ul>
         </div>
-    </nav>
+      </nav>
     )
   }
 }
