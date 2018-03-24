41252edfb994465f19c436bab65e1cc715dac332
Pedro Henrique <pedro.dorini@hotmail.com>
2018-03-24 20:57:13 -0300

    mudar texto para ingles

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -7,8 +7,8 @@ export default class Login extends Component {
       <Form>
         <FormHeader><i className="valign-wrapper material-icons left">lock</i>Login</FormHeader>
         <InputContainer>
-          <input type="text" placeholder="E-mail ou usuário"/>
-          <input type="password" placeholder="Senha"/>
+          <input type="text" placeholder="E-mail or username"/>
+          <input type="password" placeholder="Password"/>
           <button className="btn waves-effect waves-light blue darken-4">Entrar</button>
         </InputContainer>
       </Form> 
