620c5e1014d6756dac4facd2df825ec08e779bf9
Pedro Henrique <pedro.dorini@hotmail.com>
2018-03-24 20:43:22 -0300

    extrair styled components para arquivo do componente

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,25 +1,5 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-
-const Form = styled.div`
-  background-color: #f5f5f5;
-  max-width: 290px;
-  margin: 150px auto 50px;
-  border-radius: 3px;
-  box-sizing: border-box;
-  transform-origin: center bottom;
-  border-radius: 5px;
-`
-const InputContainer = styled.div`
-  padding: 15px;
-  display: inline-block;
-`
-const FormHeader  = styled.div`
-  background-color: #0d47a1;
-  padding: 15px;
-  width: 100%;
-  color: #fff;
-`
+import  { Form, InputContainer, FormHeader } from './styles'
 
 export default class Login extends Component {
   render() {
