eef5b1556e85ceeba9a2011110d58866514a680c
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-04-03 19:11:27 -0300

    Refazer componente de rotas

---
+++ src/components/Routes/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Switch, Route } from 'react-router'
+
+import PrivateRoute from '@components/PrivateRoute'
+
+import Login from '@views/Login'
+import Home from '@views/Home'
+
+class Routes extends Component {
+  render () {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route path='/login' component={ Login } />
+            <PrivateRoute
+              authed={ logged }
+              exact
+              path='/'
+              component={ Home }
+            />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+const mapStateToProps = ({}) => ({});
+
+export default connect(mapStateToProps)(Routes)
