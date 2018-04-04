e8c0b2e6b59fd86ec862a439cef17b152366deee
fernando.groeler <fernando.groeler@gmail.com>
2018-04-03 22:18:50 -0300

    Criando label and input

--- src/components/utils/form/Input/index.js
+++ src/components/utils/form/Input/index.js
@@ -1,9 +1,99 @@
+
+/*import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const DefaultInput = styled.input`
+  
+`
+
+const ErrorInput = styled.input`
+  border-bottom: 1px solid red !important;
+  box-shadow: 0 1px 0 0 red !important
+`
+
+const ErrorLabel = styled.span`
+  color: red
+`
+
+export default class TextInput extends Component {
+  static propTypes = {
+    classes: PropTypes.string,
+    name: PropTypes.string,
+    label: PropTypes.string,
+    disabled: PropTypes.bool,
+    placeholder: PropTypes.string,
+    leftIcon: PropTypes.string,
+    error: PropTypes.string,
+    false: PropTypes.bool,
+    onChange: PropTypes.func,
+    onBlur: PropTypes.func,
+    onFocus: PropTypes.func,
+    onClick: PropTypes.func
+  }
+
+  static defaultProps = {
+    classes: null,
+    name: null,
+    label: null,
+    disabled: false,
+    placeholder: null,
+    leftIcon: null,
+    error: null,
+    onChange: () => null,
+    onBlur: () => null,
+    onFocus: () => null,
+    onClick: () => null
+  }
+
+  render () {
+    const {
+      name,
+      label,
+      leftIcon,
+      classes,
+      error,
+      ...rest
+    } = this.props
+    const Input = error ? ErrorInput : DefaultInput
+    return (
+      <div className='input-field'>
+        {leftIcon && <i className='material-icons prefix'>{leftIcon}</i>}
+        {label && <label className={rest.value ? 'active': ''} htmlFor={name}>{label}</label>}
+        <Input
+          {...rest}
+          className={classes}
+          type='text'
+        />
+        {error && <ErrorLabel>{error}</ErrorLabel>}
+      </div>
+    )
+  }
+}
+*/
+
+
+
+
 import React from 'react'
 
 export default props => (
-  <input {...props.input}
-    className='form-control'
-    placeholder={props.placeholder}
-    readOnly={props.readOnly}
-    type={props.type} />
+  <div className='row'>
+    <div className='input-field col s6'>
+      <input {...props.input}
+        value={props.value}
+        id={props.id}
+        type={props.type}
+        className='validate'
+        readOnly={props.readOnly}
+        placeholder={props.placeholder}
+        />
+
+      <label {...props.label}
+        className={props.value ? 'active' : ''}
+        for={props.for}>
+      {props.caption}
+      </label>
+    </div>
+  </div>
 )
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Card, Text } from './styles'
+import Input from '@components/utils/form/Input'
 
 export default class Home extends Component {
   render() {
     return (
       <Card>
         <div>
-          <Text className="row center"><i className="material-icons large">book</i>Cadmus</Text>
+          <Text className="row center"><i className="material-icons large">book</i>Cadmus</Text>   
+          <Input id='primeiro_nome' type='text' for='primeiro_nome' caption='abc'/>
         </div>
       </Card>
     )
