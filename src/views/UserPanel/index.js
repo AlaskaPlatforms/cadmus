49f6ec1ad3165714a8906c69d011d3675993d02e
Andre Tosetto <andreftosetto@gmail.com>
2018-04-17 19:55:46 -0300

    correções

--- src/views/UserPanel/index.js
+++ src/views/UserPanel/index.js
@@ -3,15 +3,16 @@ import { SideBar, Items , Item, Container, Panel, Header, UserIcon } from './sty
 import Input from '@utils/form/Input'
 import TextArea from '@utils/form/TextArea'
 
+
 export default class  UserPanel extends Component {
 
   render(){
 
     return (
       <div>
-        <SideBar className="z-depth-2">
+        <SideBar className='z-depth-2'>
           <Items>
-            <Item className="nav-link tooltiped">
+            <Item className='nav-link tooltiped'>
               Informacoes de usuario
             </Item>
             <Item>
@@ -21,16 +22,16 @@ export default class  UserPanel extends Component {
         </SideBar>
 
         <UserIcon>
-            <i class="medium material-icons">account_box</i>
+            <i class='medium material-icons'>account_box</i>
         </UserIcon>
-        <Container className="blue-grey lighten-5">
+        <Container className='blue-grey lighten-5'>
           
-        <Header>Informacoes do usuario</Header>
+        <Header>User Info</Header>
           <Panel>
-            <form action="">
-              <Input type="text" placeholder="Nome de usuario" name="usernickname" />
-              <Input type="text" placeholder="Nome" name = "username"/>
-              <Input type="date" placeholder="Data de nascimento" name ="userbirth"/>
+            <form action=''>
+              <Input type='text' label='UserName' name='usernickname' />
+              <Input type='text' label='Name' name ='username'/>
+              <Input type='text' label='Email' name='email' />
             </form>
           </Panel>
         </Container>
@@ -38,3 +39,12 @@ export default class  UserPanel extends Component {
     )
   }
 }
+/*
+<Input type="text" class="datepicker" label="birth" name="birth" />
+id={props.id}
+      type={props.type}
+      name={props.name}
+      readOnly={props.readOnly}
+      value={props.value}
+      onChange={props.onChange}
+*/ 
