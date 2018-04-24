8fde9122229c82d44ff51e1ca05872a442252744
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-04-24 16:03:44 -0300

    Adicionar animação nova

--- src/assets/theme/animations.js
+++ src/assets/theme/animations.js
@@ -24,6 +24,18 @@ export const disappear = keyframes`
   }
 `
 
+export const appearAndSlideDown = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(-15px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`
+
 export const logoSlideMobile = keyframes`
   0% {
     left: 50%;
