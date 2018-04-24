67bd6080049bf1da1150864f4f4ba1ee58222ec9
Pedro Henrique <pedro.dorini@hotmail.com>
2018-04-24 13:41:24 -0300

    adicionar retorno implicito

--- src/services/API.js
+++ src/services/API.js
@@ -10,9 +10,7 @@ const create = (baseURL = config.apiURL) => {
 
   const checkApiStatus = () => api.get('/')
   const authUser = ({ userName, password }) => api.post(`${baseURL}/loginService/loginMobile`, { userName, password }).then(response => response)
-  const addBook = ({ book }) => {
-    return api.post(`${baseURL}/api/v1/book`, { title: book.title, description: book.description })
-  }
+  const addBook = ({ book }) => api.post(`${baseURL}/api/v1/book`, { title: book.title, description: book.description })
   
   return {
     checkApiStatus,
