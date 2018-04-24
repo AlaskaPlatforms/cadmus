fd96636b63ca953fc68c4de7f3d09661e81ff2bd
Pedro Henrique <pedro.dorini@hotmail.com>
2018-04-24 20:15:54 -0300

    adicionar botões de edição de texto no editor

--- src/views/Chapter/index.js
+++ src/views/Chapter/index.js
@@ -15,17 +15,55 @@ class Chapter extends Component {
     this.setState({ editorState })
   }
 
-  onBoldClick = () => {
-    this.handleChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  onBoldClick = (mode) => {
+    switch (mode) {
+      case 'BOLD':
+        this.handleChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
+        break
+      case 'UNDERLINE':
+        this.handleChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'))
+        break
+      case 'ITALIC':
+        this.handleChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'))
+        break
+      case 'CODE':
+        this.handleChange(RichUtils.toggleInlineStyle(this.state.editorState, 'CODE'))
+        break
+      case 'BLOCKQUOTE':
+        this.handleChange(RichUtils.toggleBlockType(this.state.editorState, 'blockquote'))
+        break
+    }
+
   }
+
+  _renderButton = (icon, mode) => (
+    <Button size='small' color='primary' onClick={ () => this.onBoldClick(mode) }>
+      <i class="material-icons">{icon}</i>
+    </Button>
+  )
+
+  _renderEditorButtons = () => (
+    <div>
+      {this._renderButton('format_bold', 'BOLD')}
+      {this._renderButton('format_underlined', 'UNDERLINE')}
+      {this._renderButton('format_italic', 'ITALIC')}
+      {this._renderButton('code', 'CODE')}
+      {this._renderButton('format_quote', 'BLOCKQUOTE')}
+    </div>
+  )
 
   render () {
     return (
       <Container>
         <Header>Escrevendo capítulo...</Header>
         <InnerContainer>
-          <Button color='primary' onClick={ this.onBoldClick }>Bold</Button>
-          <Editor editorState={ this.state.editorState } onChange={ this.handleChange }/>
+          {this._renderEditorButtons()}
+          <Editor
+            editorState={ this.state.editorState }
+            onChange={ this.handleChange }
+            placeholder="Comece a escrever..."
+            spellCheck={true}
+          />
         </InnerContainer>
       </Container>
     )
