0762099feea761b5578389c520a8f4c7dbe330f4
Pedro Henrique <pedro.dorini@hotmail.com>
2018-04-24 13:39:29 -0300

    melhorar auth sagas e adicionar try/catch no book sagas

--- src/redux/sagas/book.js
+++ src/redux/sagas/book.js
@@ -2,11 +2,15 @@ import { call, put} from 'redux-saga/effects'
 import { Creators } from '../actions'
 
 export function* addBook (api, { book }) {
-  const { data, ok } = yield call(api.addBook, { book })
-  if (ok) {
-    yield put(Creators.addBookSuccess(data))
-  } else {
-    const { error } = data
+  try {
+    const { data, ok } = yield call(api.addBook, { book })
+    if (ok) {
+      yield put(Creators.addBookSuccess(data))
+    } else {
+      const { error } = data
+      yield put(Creators.addBookFailure(error))
+    }
+  } catch (error) {
     yield put(Creators.addBookFailure(error))
   }
 }
